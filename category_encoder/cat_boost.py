bea8cab569383100ff9ab4ed52aeec42d1f2151a
Richard McQueary <56047849+richardMcQueary@users.noreply.github.com>
2021-12-21 14:41:25 -0500

    Update and rename category_encoders/cat_boost.py to category_encoder/cat_boost.py

--- category_encoder/cat_boost.py
+++ category_encoder/cat_boost.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-import category_encoders.utils as util
+import category_encoder.utils as util
 from sklearn.utils.random import check_random_state
 
 __author__ = 'Jan Motl'
@@ -51,7 +51,7 @@ class CatBoostEncoder(BaseEstimator, util.TransformerWithTargetMixin):
 
     Example
     -------
-    >>> from category_encoders import *
+    >>> from category_encoder import *
     >>> import pandas as pd
     >>> from sklearn.datasets import load_boston
     >>> bunch = load_boston()
